c5b584eca2636a4036c1ac679f47c4e01e5d0aee
Tom VanAntwerp <vanantwerp@gmail.com>
2015-03-09 11:34:41 -0400

    Make data formatting more modular

--- js/countyMap.js
+++ js/countyMap.js
@@ -1,15 +1,15 @@
 var width = 580,
-    height = 450;
+    height = 450,
 
-var svg = d3.select("body").append("svg")
-    .attr("width", width)
-    .attr("height", height);
+    svg = d3.select("body").append("svg")
+        .attr("width", width)
+        .attr("height", height);
 
 // Define increments for data scale
 var dataScale = [],
-    min = 80, //Floor for the first step
-    max = 125, //Anything above the max is the final step
-    steps = 10,
+    min = 0, //Floor for the first step
+    max = 0.4, //Anything above the max is the final step
+    steps = 5,
     increment = (max-min)/(steps-1);
 // Create the scale of data values from min to max by # of steps
 for (var step = 0; step < steps; step++) {
@@ -20,12 +20,13 @@ for (var step = 0; step < steps; step++) {
 var colors = [],
     borderColor = "#fff", //Color of borders between states
     noDataColor = "#ccc", //Color applied when no data matches an element
-    lowBaseColor = "#f15a24", //Color applied at the end of the scale with the lowest values
-    highBaseColor = "#29abe2"; //Color applied at the end of the scale with the highest values
-var scaleColor = d3.scale.linear()
-  .domain([0,steps-1])
-  .range([lowBaseColor,highBaseColor])
-  .interpolate(d3.interpolateRgb); //Don't like the colors you get? Try interpolateHcl or interpolateHsl!
+    lowBaseColor = "#fde0dd", //Color applied at the end of the scale with the lowest values
+    highBaseColor = "#c51b8a",
+     //Color applied at the end of the scale with the highest values
+    scaleColor = d3.scale.linear()
+        .domain([0,steps-1])
+        .range([lowBaseColor,highBaseColor])
+        .interpolate(d3.interpolateHcl); //Don't like the colors you get? Try interpolateHcl or interpolateHsl!
 
 // Create basic legend and add generated colors to the 'colors' array
 // Should replace this with D3.js Axis
@@ -37,7 +38,9 @@ var tooltip = d3.select("body").append("div")
     .attr("class", "tooltip")
     .style("position", "absolute")
     .style("opacity", 0),
+
     dataFormat = {
+        percentage: d3.format("%"),
         tens: d3.format("$,.4r"),
         hundreds: d3.format("$,.5r")
     };
@@ -60,9 +63,12 @@ var legend = svg.append("g")
     .attr("class", "legend")
     .attr("transform", "translate(0," + (height - height * 0.1) + ")");
 
+var dataPath = "data/retirees.csv",
+    dataType = dataFormat.percentage;
+
 queue()
     .defer(d3.json, "data/us.json")
-    .defer(d3.csv, "data/rpp-dollars-data.csv")
+    .defer(d3.csv, dataPath)
     .await(ready);
 
 function ready(error, us, data) {
@@ -77,8 +83,8 @@ function ready(error, us, data) {
 
     data.forEach(function(d){
         d3.select("#county" + d.id)
-            .style("fill", mapColor(parseFloat(d.rpp)))
-            .on("mouseover", function(){ return addTooltip(d.metroName, d.rpp); })
+            .style("fill", mapColor(parseFloat(d.retirementIncomePercentage)))
+            .on("mouseover", function(){ return addTooltip(d.name, d.retirementIncomePercentage); })
             .on("mouseout", function(d){ tooltip.transition().duration(200).style("opacity",0); });
     });
 
@@ -94,14 +100,14 @@ function ready(error, us, data) {
 
 var adjustment = d3.scale.linear()
                 .domain([0, width])
-                .range([0, 400]);
+                .range([0, 150]);
 
 function addTooltip(label, number){
   tooltip.transition()
     .duration(200)
     .style("opacity", 0.9);
   tooltip.html(
-    label + ": " + ((parseFloat(number) < 100) ? dataFormat.tens(parseFloat(number)) : dataFormat.hundreds(parseFloat(number)))
+    label + ": " + dataType(parseFloat(number))
   )
     .style("left", (d3.event.pageX - adjustment(d3.event.pageX)) + "px")
     .style("top", (d3.event.pageY + 50) + "px");
@@ -115,7 +121,7 @@ function drawLegend() {
 
     for (var i = 0, j = colors.length; i < j; i++){
         var fill = colors[i];
-        var label = "$" + (min + increment*i) + ((i === j - 1) ? "+" : "-" + (min + increment*(i+1)));
+        var label = dataType(min + increment*i) + ((i === j - 1) ? "+" : "-" + dataType(min + increment*(i+1)));
         legendData[i+1]= {"color": fill, "label": label};
     }
 
